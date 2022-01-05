dbd9c89e9585331a03a7411a1263e2108fe738cd
Kort Driessene <driessenkort@gmail.com>
2022-01-05 16:33:25 -0600

    First version-2 full update

--- sleep_score_for_me/utils/ssfm_utils.py
+++ sleep_score_for_me/utils/ssfm_utils.py
@@ -1,26 +1,12 @@
 import numpy as np
-import scipy
 import pandas as pd
-import tdt
-from pathlib import Path
 import matplotlib.pyplot as plt
 import seaborn as sns
-import xarray as xr
-import yaml
-import re
+import neurodsp.plts.utils as dspu
 
-import hypnogram as hp
-from ecephys.utils import replace_outliers
-import ecephys.plot as eplt
-import ecephys.signal.timefrequency as tfr
-import ecephys.signal.kd_utils as kd
-import ecephys.signal.kd_plotting as kp
-import ecephys.signal.kd_pax as kpx
-import ecephys.xrsig.hypnogram_utils as xrhyp
-import tdt_xarray as tx
+import kd_analysis.main.kd_utils as kd
 
-from sklearn.decomposition import PCA
-from neurodsp.plts.utils import check_ax
+
 bp_def = dict(delta=(0.5, 4), theta=(4, 8), sigma = (11, 16), beta = (13, 30), low_gamma = (30, 55), high_gamma = (65, 90), omega=(300, 700))
 
 hypno_colors = {
@@ -37,40 +23,162 @@ hypno_colors = {
     "Unsure": "white",
     }
 
+def build_hypno_for_me(states_and_times):
+    """ states_and_times --> series with only the timepoints corresponding sleep states (i.e. data that has already been 'scored')"""
+    
+    # Get boolean series for each state
+    nrem_bool = states_and_times == 'NREM'
+    wake_bool = states_and_times == 'Wake'
+    rem_bool = states_and_times == 'REM'
 
-def _infer_bout_start(df, bout):
-    """Infer a bout's start time from the previous bout's end time.
+    # Use the boolean series to get start and end times for each state
+    nrem_sne = starts_and_ends(nrem_bool)
+    wake_sne = starts_and_ends(wake_bool)
+    rem_sne = starts_and_ends(rem_bool)
+
+    # Then we convert the start and end times for each state to a partial hypnogram
+    nrem_hyp = pd.DataFrame(columns = ['state', 'end_time', 'start_time', 'duration'])
+    nrem_hyp[['start_time', 'end_time']] = nrem_sne
+    nrem_hyp['duration'] = nrem_hyp.end_time - nrem_hyp.start_time
+    nrem_hyp['state'] = 'NREM'
+
+    wake_hyp = pd.DataFrame(columns = ['state', 'end_time', 'start_time', 'duration'])
+    wake_hyp[['start_time', 'end_time']] = wake_sne
+    wake_hyp['duration'] = wake_hyp.end_time - wake_hyp.start_time
+    wake_hyp['state'] = 'Wake'
+
+    rem_hyp = pd.DataFrame(columns = ['state', 'end_time', 'start_time', 'duration'])
+    rem_hyp[['start_time', 'end_time']] = rem_sne
+    rem_hyp['duration'] = rem_hyp.end_time - rem_hyp.start_time
+    rem_hyp['state'] = 'REM'
+
+    #Then we concat those and sort by the start_time 
+    fin_hypno = pd.concat([nrem_hyp, wake_hyp, rem_hyp])
+    fin_hypno = fin_hypno.sort_values('start_time').reset_index(drop=True)
+       
+    return fin_hypno
+
+def starts_and_ends(s, minimum_duration=np.timedelta64(3, 's')):
+    start_times = np.empty(0)
+    end_times = np.empty(0)
+    period = s.index[1] - s.index[0]
+
+    s_trues = s[s==True]
+    ix = s_trues.index
+    ix_counter = np.arange(0, len(ix))
+
+    try:
+        for i in ix_counter:
+            if (ix[i] - period) != ix[i-1]:
+                start_times = np.append(start_times, ix[i])
+            if (ix[i] + period) != ix[i+1]:
+                end_times = np.append(end_times, (ix[i] + period))
+            elif np.logical_and((ix[i] + period) == ix[i+1], (ix[i] - period) == ix[i-1]):
+                pass 
+    except IndexError:
+        print('passing indexing error (starts_and_ends)')
+        pass
+    end_times = np.append(end_times, (ix[ix_counter.max()] + period))
+    return [(start_time, end_time)
+            for start_time, end_time in zip(start_times, end_times)
+            if end_time >= (start_time + minimum_duration)]
+
+
+"""
+PLOTTING FUNCTIONS
+------------------
+"""
+
+def threshplot(data, time=None, percentiles=[50, 60, 65, 70]):
+    
+    #%matplotlib inline
+    # Plot the threshold options for scoring out the NREM: 
+    f, h_ax = plt.subplots(figsize=(40, 15))
+    h_ax = sns.histplot(data=data, ax=h_ax, bins=50)
+    h_ax.axvline(np.percentile(data, percentiles[0]), color='magenta')
+    h_ax.axvline(np.percentile(data, percentiles[1]), color='b')
+    h_ax.axvline(np.percentile(data, percentiles[2]), color='forestgreen')
+    h_ax.axvline(np.percentile(data, percentiles[3]), color='r')
+    plt.show()
+    
+    f, lin_ax = plt.subplots(figsize=(40, 15))
+    lin_ax = sns.lineplot(x=time, y=data, ax=lin_ax)
+    lin_ax.axhline(np.percentile(data, percentiles[0]), color='magenta')
+    lin_ax.axhline(np.percentile(data, percentiles[1]), color='b')
+    lin_ax.axhline(np.percentile(data, percentiles[2]), color='forestgreen')
+    lin_ax.axhline(np.percentile(data, percentiles[3]), color='r')
+    plt.show()
+    return h_ax, lin_ax
+
+def plot_hypno_for_me(hypno, spg, emg_spg, bp_def, chan=2, smooth=False):
+    fig, (m, d, g) = plt.subplots(ncols=1, nrows=3, figsize=(35,15))
+    emg_spg = emg_spg.sel(channel=1)
+    try:
+        spg = spg.sel(channel=chan)
+    except KeyError:
+        print('channel already selected, passing key error (plot_hypno_for_me)')
+    
+    #plot muscle activity
+    emg_bp = kd.get_bandpower(emg_spg, bp_def['omega'])
+    if smooth==True:
+        emg_bp = kd.get_smoothed_da(emg_bp, smoothing_sigma=12)
+    sns.lineplot(x=spg.datetime, y=emg_bp, color='black', ax=m) 
+    shade_hypno_for_me(hypnogram=hypno, ax=m)
+    m.set_title('Muscle Activity (Full Spectrum)')
+
+    #plot delta power
+    delta = kd.get_bandpower(spg, bp_def['delta'])
+    if smooth==True:
+        delta = kd.get_smoothed_da(delta, smoothing_sigma=12)
+    sns.lineplot(x=delta.datetime, y=delta, color='black', ax=d)
+    shade_hypno_for_me(hypnogram=hypno, ax=d)
+    d.set_title('EEG-'+str(chan)+' Delta Bandpower')
+
+    #plot gamma power
+    gamma = kd.get_bandpower(spg, bp_def['high_gamma'])
+    if smooth==True:
+        gamma = kd.get_smoothed_da(gamma, smoothing_sigma=12)
+    sns.lineplot(x=spg.datetime, y=gamma, color='black', ax=g)
+    shade_hypno_for_me(hypnogram=hypno, ax=g)
+    g.set_title('EEG-'+str(chan)+' Gamma Bandpower')
+
+    return m, d, g
+
+def shade_hypno_for_me(
+    hypnogram, ax=None, xlim=None
+):
+    """Shade plot background using hypnogram state.
 
     Parameters
     ----------
-    h: DataFrame, (n_bouts, ?)
-        Hypogram in Visbrain format with 'start_time'.
-    row: Series
-        A row from `h`, representing the bout that you want the start time of.
-
-    Returns
-    -------
-    start_time: float
-        The start time of the bout from `row`.
+    hypnogram: pandas.DataFrame
+        Hypnogram with with state, start_time, end_time columns.
+    ax: matplotlib.Axes, optional
+        An axes upon which to plot.
     """
-    if bout.name == 0:
-        start_time = 0.0
-    else:
-        start_time = df.loc[bout.name - 1].end_time
+    xlim = ax.get_xlim() if (ax and not xlim) else xlim
 
-    return start_time
+    ax = dspu.check_ax(ax)
+    for bout in hypnogram.itertuples():
+        ax.axvspan(
+            bout.start_time,
+            bout.end_time,
+            alpha=0.3,
+            color=hypno_colors[bout.state],
+            zorder=1000,
+            ec="none",
+        )
 
-def load_hypno(path, st):
-    """Load a Visbrain formatted hypnogram."""
-    df = pd.read_csv(path, sep="\t", names=["state", "end_time"], comment="*")
-    df["start_time"] = df.apply(lambda row: _infer_bout_start(df, row), axis=1)
-    df["duration"] = df.apply(lambda row: row.end_time - row.start_time, axis=1)
-    return to_datetime(df, st)
+    ax.set_xlim(xlim)
+    return ax
 
-
-def to_datetime(df, start_datetime):
-    df = df.copy()
-    df["start_time"] = start_datetime + pd.to_timedelta(df["start_time"], "s")
-    df["end_time"] = start_datetime + pd.to_timedelta(df["end_time"], "s")
-    df["duration"] = pd.to_timedelta(df["duration"], "s")
-    return hp.DatetimeHypnogram(df)
+def compare_hypnos_for_me(spg, ssfm_hyp, your_hyp):
+    f, (ssfm_hyp_ax, your_hyp_ax) = plt.subplots(nrows=2, ncols=1, figsize=(35, 15))
+    spg = kd.get_bandpower(spg, (0.5, 4))
+    ssfm_hyp_ax = sns.lineplot(x=spg.datetime, y=spg, ax=ssfm_hyp_ax)
+    ssfm_hyp_ax.set_title('SSFM Hypnogram')
+    your_hyp_ax = sns.lineplot(x=spg.datetime, y=spg, ax=your_hyp_ax)
+    your_hyp_ax.set_title('Your Hypnogram')
+    shade_hypno_for_me(ssfm_hyp, ax=ssfm_hyp_ax)
+    shade_hypno_for_me(your_hyp, ax=your_hyp_ax)
+    return ssfm_hyp_ax, your_hyp_ax
